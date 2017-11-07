fe4c4cb1dd4702b1fc2c45fb1f3b2fbbf63b9071
Adrian Mendoza <adrianmendozar88@hotmail.com>
2017-11-06 21:46:20 -0600

    Modificaciones al Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,10 @@ import {
 
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'About', component: AboutComponent },
   { path: 'Item', component: ItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});
