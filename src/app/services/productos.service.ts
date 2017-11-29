770ab78111e6214f63c66b9ad99a853458edca25
Adrian Mendoza <adrianmendozar88@hotmail.com>
2017-11-28 22:41:28 -0600

    pagina de busqueda creada

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,10 +5,45 @@ import {Http} from "@angular/http"
 export class ProductosService {
 
   productos:any[] = [];
-  cargando:boolean = false;
+  productos_filtrado:any = [];
+  cargando:boolean = true;
 
   constructor(private http:Http) {
     this.cargar_productos();
+  }
+
+  public buscar_productos( termino:string){
+      console.log("buscando producto");
+      console.log(this.productos.length);
+
+      if(this.productos.length === 0){
+        this.cargar_productos().then( ()=>{
+          //termino la carga
+          this.filtrar_productos(termino);
+        });
+      }else{
+        this.filtrar_productos(termino);
+
+      }
+
+
+  }
+
+  private filtrar_productos(termino:string){
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase()
+
+    this.productos.forEach( prod =>{
+
+      if(prod.categoria.indexOf(termino) >=0 || prod.titulo.toLowerCase().indexOf(termino) >=0 ){
+        this.productos_filtrado.push(prod);
+        console.log(prod);
+      }
+
+
+      //console.log(prod);
+    })
   }
 
   public cargar_producto(cod:string){
@@ -19,13 +54,22 @@ export class ProductosService {
 
     this.cargando = true;
 
-          this.http.get('https://deasislaboratorio-49308.firebaseio.com/productos_idx.json')
-            .subscribe(res => {
+    let promesa = new Promise( ( resolve, reject) =>{
 
-              //console.log( res.json());
-              this.cargando = false;
-              this.productos = res.json();
-            });
+      this.http.get('https://deasislaboratorio-49308.firebaseio.com/productos_idx.json')
+        .subscribe(res => {
+
+          //console.log( res.json());
+          this.cargando = false;
+          this.productos = res.json();
+          resolve();
+        });
+    });
+
+    return promesa;
+
+
+
       }
 
 }
